9bb83dea86bae83e92cb5466dc3d5960736727fd
Manish Mehta <mehta.manish@tftus.com>
2018-06-28 14:31:28 +0530

    get ALL post finished

--- controller/postCont/index.js
+++ controller/postCont/index.js
@@ -50,7 +50,7 @@ exports.getpost = (req,res)=>{
 };
 
 exports.getAllPost = (req,res)=>{
-    db.profile.findAll({
+    /*db.post.findAll({
         include: [{ model: db.post,
                     include: [{ 
                                 model: db.comment
@@ -58,7 +58,7 @@ exports.getAllPost = (req,res)=>{
                               {
                                   model: db.postReactions
                               }
-                            ]
+                            ],
                 }]
     }).then(profile => {
         if(!profile)
@@ -89,7 +89,12 @@ exports.getAllPost = (req,res)=>{
         res.status(200).send({success:true, data:resObj});
     }).catch(err => { 
         util.errorHandler(err,req,res)
-    })
+    })*/
+    db.sequelize.query("SELECT postTable.* ,pro.firstName ,pro.lastName, pro.country, pro.city,pro.picture AS userImage FROM (SELECT y.* ,COUNT(sh.id) shCount FROM ( SELECT x.* ,COUNT(rec.id) recId FROM ( SELECT a.id ,a.content ,a.createdAt ,a.profileId AS uid,a.imageLink AS postImage,COUNT(c.content) AS 'count' FROM posts a LEFT JOIN comments c ON a.id = c.postId GROUP BY a.id ORDER BY a.createdAt DESC LIMIT 10) x LEFT JOIN postReactions rec ON rec.postId = x.id GROUP BY x.id ) y LEFT JOIN shares sh ON sh.postId = y.id GROUP BY y.id) postTable LEFT JOIN profiles pro ON pro.id = postTable.uid", { type: db.sequelize.QueryTypes.SELECT})
+    .then(users => {
+        res.send(users)
+    // We don't need spread here, since only the results will be returned for select queries
+  })
 };
 
 exports.updatePost = (req,res)=>{
--- sqlExtra/insert.sql
+++ sqlExtra/insert.sql
@@ -1,27 +1,27 @@
 #for profiles dummy data
 
 INSERT INTO `chameleon`.`profiles`
-(`id` ,`alias` ,`firstName`,`lastName`,`image`,`gender`,`email`,`cellularNumber`,`country`,`city`,`zipCode`,`industry`,`currentPosition`,`createdAt`,`updatedAt`)
+(`id` ,`authId` ,`firstName`,`lastName`,`picture`,`gender`,`email`,`cellularNumber`,`country`,`city`,`zipCode`,`industry`,`currentPosition`,`createdAt`,`updatedAt`)
 VALUES
 ("1","user1","f_name1","l_name1","http://www.venmond.com/demo/vendroid/img/avatar/big.jpg","male","user1@yahoo.com","999999991","india","pune","471105","I.T","developer","2018-06-15 03:14:07 ","2018-06-15 03:14:07 "); 
 
 INSERT INTO `chameleon`.`profiles`
-(`id` ,`alias` ,`firstName`,`lastName`,`image`,`gender`,`email`,`cellularNumber`,`country`,`city`,`zipCode`,`industry`,`currentPosition`,`createdAt`,`updatedAt`)
+(`id` ,`authId` ,`firstName`,`lastName`,`picture`,`gender`,`email`,`cellularNumber`,`country`,`city`,`zipCode`,`industry`,`currentPosition`,`createdAt`,`updatedAt`)
 VALUES
 ("2","user2","f_name2","http://keenthemes.com/preview/metronic/theme/assets/pages/media/profile/profile_user.jpg","l_name2","female","user2@yahoo.com","999999992","nepal","kathmandu","784851","I.T","support","2018-06-15 03:14:07 ","2018-06-15 03:14:07 "); 
 
 INSERT INTO `chameleon`.`profiles`
-(`id` ,`alias` ,`firstName`,`lastName`,`image`,`gender`,`email`,`cellularNumber`,`country`,`city`,`zipCode`,`industry`,`currentPosition`,`createdAt`,`updatedAt`)
+(`id` ,`authId` ,`firstName`,`lastName`,`picture`,`gender`,`email`,`cellularNumber`,`country`,`city`,`zipCode`,`industry`,`currentPosition`,`createdAt`,`updatedAt`)
 VALUES
 ("3","user3","f_name3","l_name3","http://keenthemes.com/preview/metronic/theme/assets/pages/media/profile/profile_user.jpg","male","user3@yahoo.com","999999993","india","dehradun","248002","Management","HR","2018-06-15 03:14:07 ","2018-06-15 03:14:07 "); 
 
 INSERT INTO `chameleon`.`profiles`
-(`id` ,`alias` ,`firstName`,`lastName`,`image`,`gender`,`email`,`cellularNumber`,`country`,`city`,`zipCode`,`industry`,`currentPosition`,`createdAt`,`updatedAt`)
+(`id` ,`authId` ,`firstName`,`lastName`,`picture`,`gender`,`email`,`cellularNumber`,`country`,`city`,`zipCode`,`industry`,`currentPosition`,`createdAt`,`updatedAt`)
 VALUES
 ("4","user4","f_name4","l_name4","http://keenthemes.com/preview/metronic/theme/assets/pages/media/profile/profile_user.jpg","female","user4@yahoo.com","999999994","china","fushan","84821","Management","Executive","2018-06-15 03:14:07 ","2018-06-15 03:14:07"); 
 
 INSERT INTO `chameleon`.`profiles`
-(`id` ,`alias` ,`firstName`,`lastName`,`image`,`gender`,`email`,`cellularNumber`,`country`,`city`,`zipCode`,`industry`,`currentPosition`,`createdAt`,`updatedAt`)
+(`id` ,`authId` ,`firstName`,`lastName`,`picture`,`gender`,`email`,`cellularNumber`,`country`,`city`,`zipCode`,`industry`,`currentPosition`,`createdAt`,`updatedAt`)
 VALUES
 ("5","user5","f_name5","http://keenthemes.com/preview/metronic/theme/assets/pages/media/profile/profile_user.jpg","l_name5","male","user5@yahoo.com","999999995","america","california","21214","Marketing","senior head","2018-06-15 03:14:07 ","2018-06-15 03:14:07 "); 
 
